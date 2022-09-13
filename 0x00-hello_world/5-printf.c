629b74566a98c10d456e01e21d30ef3bb1e522ba
Tiitaniiium <okunlolatimmey007@gmail.com>
2022-09-13 15:23:13 -0700

    printf2

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,10 +5,10 @@
 *
 * Description: using the main function
 * this program prints "programming is like building a multilingual puzzle"
-*Return: 0
-*/ 
+* Return: 0
+*/
 int main(void)
-{	
+{
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-}	
+}
