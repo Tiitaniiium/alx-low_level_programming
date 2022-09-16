b1cd6fa70bd0f4490da952b5abacdbbb871538d9
Tiitaniiium <okunlolatimmey007@gmail.com>
2022-09-16 14:30:05 -0700

    mirrorsmile

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+*main - entry point
+*Description - print alphabet backwards
+*Return: 0
+*/
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch <= 'a'; ch--)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
