c20c507d38561befe3e31c4fe3d8bcfe40e42dfa
Tiitaniiium <okunlolatimmey007@gmail.com>
2022-09-16 16:05:27 -0700

    printcomb8

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,10 +10,11 @@ int main(void)
 {
 	int x;
 
-	for(x = '0'; x <= '9'; x++)
+	for (x = '0'; x <= '9'; x++)
 	{
 		putchar(x);
-		if (x < '9'){
+		if (x < '9')
+		{
 			putchar(',');
 			putchar(' ');
 		}
