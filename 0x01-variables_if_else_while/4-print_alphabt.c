54cae497e51ae35568c4b737aae5f53e73019a2a
Tiitaniiium <okunlolatimmey007@gmail.com>
2022-09-16 12:02:03 -0700

    4print commit 2

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,11 +1,17 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 
+/**
+*main - Entry point
+*Return: 0
+*/
 int main(void)
 {
 	char ch;
+
 	for (ch = 'a'; ch <= 'z'; ch++)
-	if (ch != q; ch != e)
+	if (ch != q && ch != e)
 		putchar(ch);
 	putchar('\n');
 	return (0);
