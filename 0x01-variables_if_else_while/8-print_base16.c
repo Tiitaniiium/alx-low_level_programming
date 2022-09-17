1148d2fd2ae92749cdd9f69ad4137508ca4b41f2
Tiitaniiium <okunlolatimmey007@gmail.com>
2022-09-16 17:34:08 -0700

    printbase2

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,9 +10,12 @@ int main(void)
 {
 	int N;
 	for (N = '0'; N <= '9'; N++)
+	{
 		putchar(N);
+	}
 	for (N = 'a'; N <= 'f'; N++)
+	{
 		putchar(N);
+	}
 		putchar('\n');
-	return (0);
-}
+	return (0);}
