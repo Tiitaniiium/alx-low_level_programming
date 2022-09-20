565327af34c6d356c83e5cfb3ad6c4779f761393
Tiitaniiium <okunlolatimmey007@gmail.com>
2022-09-20 16:53:58 -0700

    task1-1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*
+* void swap_int(int *a, int *b) - swaps the value of two intergers a and b
+*
+* Return : Always 0.
+*/
+
+void swap_int(int *a, int *b)
+{
+	int a;
+	int b;
+
+	a = A;
+	b = B;
+
+	*a = b;
+	*b = a;
+}
