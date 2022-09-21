41f19505b897ae4676267a345a4934b0fd050286
Tiitaniiium <okunlolatimmey007@gmail.com>
2022-09-21 16:38:08 -0700

    print comb

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+*main - Entry point.                                              
+*
+*return : always 0
+*/
+int main(void)
+{
+	int d;
+
+	for (d = 0; d <= 100; d++)
+		putchar((d / 10) + '0');
+		putchar((d % 10) + '0');
+
+		if (d != 99)
+			putchar(',');
+			putchar(' ');
+	putchar('\n');
+
+	return (0);
+}
