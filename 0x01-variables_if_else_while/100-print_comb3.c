033139554e3fd95bd13589cd9bdcf36714e68649
Tiitaniiium <okunlolatimmey007@gmail.com>
2022-09-21 16:44:07 -0700

    print -m

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,7 +3,7 @@
 #include <time.h>
 
 /**
-*main - Entry point.                                              
+*main - Entry point
 *
 *return : always 0
 */
@@ -12,12 +12,12 @@ int main(void)
 	int d;
 
 	for (d = 0; d < 100; d++)
-	putchar((d / 10) + '0');
-	putchar((d % 10) + '0');
+		putchar((d / 10) + '0');
+		putchar((d % 10) + '0');
 
 		if (d != 99)
-		putchar(',');
-		putchar(' ');
+			putchar(',');
+			putchar(' ');
 	putchar('\n');
 
 	return (0);
