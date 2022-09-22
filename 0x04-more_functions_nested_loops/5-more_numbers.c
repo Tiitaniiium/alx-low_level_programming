f1bd517db782a1637465da6c09603993252ab460
Tiitaniiium <okunlolatimmey007@gmail.com>
2022-09-22 04:29:04 -0700

    printmoremore

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+*void more_numbers- function prints 10 times the numbers, from 0 to 14
+*
+*Return: returns void
+*/
+
+void more_numbers(void)
+{
+int n;
+
+for (n == 48; n < 62; n++)
+{
+putchar(n);
+}
+putchar('\n');
+}
