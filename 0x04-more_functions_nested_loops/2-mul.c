8d381c52c5dd6db4f5f411b7c2f9260cad0c85c5
Tiitaniiium <okunlolatimmey007@gmail.com>
2022-09-22 03:02:13 -0700

    multiplc

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,9 +1,10 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
 *mul - multiplies two numbers
 *@a: first number
-*@: second number
+*@b: second number
 *Return: multiplication of a and b
 */
 
@@ -11,4 +12,3 @@ int mul(int a, int b)
 {
 return (a * b);
 }
-
