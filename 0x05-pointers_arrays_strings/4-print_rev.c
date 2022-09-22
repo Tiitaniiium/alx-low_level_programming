b0e4be6e442c7931d376a61de9f6bbe6276b0154
Tiitaniiium <okunlolatimmey007@gmail.com>
2022-09-22 16:11:35 -0700

    printrevn

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-/** 
+/**
 * print_rev - print a string in reverse
 * @s : the string to be printed in rev
 */
@@ -8,9 +8,13 @@ void print_rev(char *s)
 {
 	int  i, n;
 
-	
+	n = 0;
+	while (s[n] != '\0')
+		n++;
+
 	for (i = n - 1; i >= 0; i--)
 	{
 		_putcha r(s[i]);
 	}
+	_putchar('\n');
 }
