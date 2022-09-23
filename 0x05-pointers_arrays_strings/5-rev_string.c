91ec2d5dbdaeff902c8ba3124cbf0d01c5785b1f
Tiitaniiium <okunlolatimmey007@gmail.com>
2022-09-23 02:00:12 -0700

    stringrevr

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,9 +1,8 @@
 #include "main.h"
-
 /**
 * rev_string - s function that reverses a string
 *@s : string parameter
-*return: 
+*return: nothing
 */
 void rev_string(char *s)
 {
@@ -15,13 +14,13 @@ void rev_string(char *s)
 
 	while (s[length1] != '\0')
 		length1++;
-	
-	length2 = length1 - 1
+
+	length2 = length1 - 1;
 	for (i = 0; i < length1 / 2; i++)
-	{ 
+	{
 		tmp = s[i];
 		s[i] = s[length2];
 		s[length2] = tmp;
-		length2 -= 1
+		length2 -= 1;
 	}
 }
