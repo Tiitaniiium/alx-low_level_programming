1cbbcc7a5fca55aaa21272254ad5c94fb4d1af91
Tiitaniiium <okunlolatimmey007@gmail.com>
2022-09-23 02:44:45 -0700

    strlength

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,15 +1,15 @@
 #include "main.h"
 #include <string.h>
 /**
-* puts2 - a function that prints 
-* @str - string parameter
+* puts2 - a function that prints
+* @str: string parameter
 */
 void puts2(char *str)
 {
 	int len, i;
 
 	len = strlen(str);
-	for (i = 0; i < len; i++)
+	for (i = 0; i < len; i += 2)
 		_putchar(str[i]);
 	_putchar('\n');
 }
