c8be44e2b55935423b81fc4f2915bfe3082d2382
Tiitaniiium <okunlolatimmey007@gmail.com>
2022-09-23 02:13:48 -0700

    puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+/**
+* puts2 - a function that prints 
+* @str - string parameter
+*/
+void puts2(char *str)
+{
+	int len, i;
+
+	len = strlen(str);
+	for (i = 0; i < len; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
