0fb4108c478dfc317bcddeedb0c18647a927f193
Tiitaniiium <okunlolatimmey007@gmail.com>
2022-09-27 08:03:04 -0700

    2-Strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -3,7 +3,7 @@
 /**
  * _strchr - locates a character in a string
  *@s: string to check
- *@c: character to check for 
+ *@c: character to check for
  * Return: pointer to char if char is found else Null
  */
 char *_strchr(char *s, char c)
