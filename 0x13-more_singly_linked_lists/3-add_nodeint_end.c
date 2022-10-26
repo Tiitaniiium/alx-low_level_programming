263b06650f27d204e9b41eb20861686415b301c2
Tiitaniiium <okunlolatimmey007@gmail.com>
2022-10-26 10:27:55 -0700

    addNodeEnd

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -8,9 +8,10 @@
  *
  * Return: address of new node
  */
-listint_t *add_nodeint(listint_t **head, const int n)
+listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new;
+	listint_t *current;
 
 	if (head == NULL)
 		return (NULL);
@@ -18,7 +19,17 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
-	new->next = *head;
-	*head = new;
+	new->next = *NULL;
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+	current = *head;
+	while (current->next !=NULL)
+	{
+		current = current->next;
+	}
+	current->next = new;
 	return (new);
 }
